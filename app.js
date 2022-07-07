3ceff59c44b246c40bd35c11dbd8b1ef046d4ed5
feriteja <feriteja@gmail.com>
2022-07-07 09:37:22 +0700

    Assignment 1, type input three times

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const rl = readline.createInterface({
 });
 
 rl.question("What is yout name: ", (name) => {
-  rl.question("What is your contact", (number) => {
-    rl.question("What is your email", (email) => {
+  rl.question("What is your contact: ", (number) => {
+    rl.question("What is your email: ", (email) => {
       console.log(
         `Your name is ${name},\nwith number: ${number}, \nyour email address: ${email}`
       );
